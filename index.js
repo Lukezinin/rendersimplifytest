e79e30b067c7fe808548c113f40af8308cddab7d
Lukezinin <200152244+Lukezinin@users.noreply.github.com>
2026-04-23 18:15:01 -0300

    Refactor port configuration in index.js
    Updated server configuration to use environment variable for HTTP port.

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const NodeMediaServer = require('node-media-server');
+const port = process.env.PORT || 8000;
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 1935, // Porta interna padrão
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port: process.env.PORT || 8000,
+    port: port,
     allow_origin: '*'
   }
 };
-
-var nms = new NodeMediaServer(config);
-nms.run();
-
-console.log("Servidor de Stream Rodando...");
